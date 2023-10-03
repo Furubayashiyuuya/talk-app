ea6d84e4a740737d4c7f8b051ad58462ac341469
Furubayashiyuuya <cbyf1210195@gn.iwasaki.ac.jp>
2023-10-03 15:07:58 +0900

    component01

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,3 +1,4 @@
+/*
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -16,3 +17,21 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+*/
+import firebase from "firebase/compat/app";
+import "firebase/compat/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCTz7WRVfaQergkV7Szr6gmVarhBHYCnpI",
+  authDomain: "talk-95e0a.firebaseapp.com",
+  databaseURL: "https://talk-95e0a-default-rtdb.firebaseio.com",
+  projectId: "talk-95e0a",
+  storageBucket: "talk-95e0a.appspot.com",
+  messagingSenderId: "348990437531",
+  appId: "1:348990437531:web:beb9f0e897cae19c322e3e",
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+
+export default database;
