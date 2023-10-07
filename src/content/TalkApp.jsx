bac5f717b6e6e91695d8883ae6ac44a859d15448
Furubayashiyuuya <cbyf1210195@gn.iwasaki.ac.jp>
2023-10-07 18:31:42 +0900

    css-update

--- src/content/TalkApp.jsx
+++ src/content/TalkApp.jsx
@@ -3,9 +3,14 @@ import TalkSide from "./TalkSide";
 import  "./TalkApp.css";
 function TalkApp() {
   return (
+    <>
     <div className="talk">
+    <div className="title">
+        <h1>Talk</h1>
+      </div>
       <TalkSide />
     </div>
+    </>
   );
 }
 export default TalkApp;
